3f17282e1b9c62a93740eb15bd6d298168c26b6e
natalete <natsalete14@gmail.com>
2024-05-08 16:29:44 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form-deposito");
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+console.log(form);
