fff003589c86d735467b3dade4af02117d55f8b7
natalete <natsalete14@gmail.com>
2024-05-08 17:01:25 -0300

    feat: adicão de eventos a elementos HTML

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const form = document.getElementById("form-deposito");
 
 function validaNome(nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
-    
+  const nomeComoArray = nomeCompleto.split(" ");
+  return nomeComoArray.length >= 2;
 }
 
 form.addEventListener("submit", function (e) {
+  let formeEValido = false;
   e.preventDefault();
+
+  const nomeBeneficiario = document.getElementById("nome-beneficiario");
+  const numeroContaBeneficiario = document.getElementById("numero-conta");
+  const valorDeposito = document.getElementById("valor-deposito");
+  const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value} `;
+
+  formeEValido = validaNome(nomeBeneficiario.value);
+  if (formeEValido) {
+    alert(mensagemSucesso);
+    nomeBeneficiario.value = "";
+    numeroContaBeneficiario.value = "";
+    valorDeposito.value = "";
+  } else alert("O nome não está completo!");
 });
 
 console.log(form);
